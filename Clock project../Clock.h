db6938c2a5189d78bda145c3a8c7bda811375935
perecke <perecpc@gmail.com>
2017-04-16 19:54:21 +0900

    Update and rename ClockClass.h to Clock project../Clock.h

--- Clock project../Clock.h
+++ Clock project../Clock.h
@@ -1,10 +1,3 @@
-//
-//  ClockClass.h
-//  interfaceElderlySupport
-//
-//  Created by Kubota Naoyuki on 2017/03/03.
-//  Copyright © 2017年 Kubota Naoyuki. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
