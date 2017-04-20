c448cc9770cb7abcc9a31f196534e59a768ec71b
perecke <perecpc@gmail.com>
2017-04-20 11:46:31 +0900

    Update Clock.m

--- Clock project../Clock.m
+++ Clock project../Clock.m
@@ -8,7 +8,7 @@
     UIGraphicsBeginImageContext(image.size);
     [image drawAtPoint:CGPointMake(0,0)];
     
-    //Smal clock ticker thing
+    //Hour ticker
    
     int absoluteValue = _hour;
     int clockValue = 0;
@@ -16,8 +16,8 @@
     if (_hour > 12) {
         absoluteValue = (_hour - 12);
     }
-    if (absoluteValue >= 3 && absoluteValue <= 12) { //hour hour
-        //elojel = -1;
+    //Check if we are between 3 and 12 because of the degree calculation
+    if (absoluteValue >= 3 && absoluteValue <= 12) { 
         clockValue = absoluteValue - 3;
     }
     
@@ -27,23 +27,23 @@
     }
     CGContextRef cotext = UIGraphicsGetCurrentContext();
     CGContextSaveGState(cotext);
-    //int szorzo = absoluteValue / 2;
+    //We are going to move the hour ticker 30 degree every time an hour has passed by
     double angle =(elojel * clockValue * 30) * (M_PI/180); //30
-    //NSLog(@"The angel %d and cock is %i",angle,clockValue);
     
+    //If it is past 30 secoond, we moce the hour ticker to make the clock more easy to understand
     if (_minute > 30) {
         angle += (elojel * 15) * (M_PI/180);
     }
     
     CGContextSetLineWidth(cotext, 10.0);
-    //length = 80
+    //Calculate the angle of the ticker where the length is 60 pixel and the image is the clock`s main image
     double endX = cos(angle) * 60 + image.size.width/2;
     double endY = sin(angle) * 60 + image.size.height / 2;
-    CGContextMoveToPoint(cotext,endX,endY ); //(image.size.width/2), 60
+    CGContextMoveToPoint(cotext,endX,endY ); 
     CGContextAddLineToPoint(cotext, image.size.width/2, image.size.height / 2);
     /////////////////////////
     
-    //Bigger ticker
+    //Minute ticker
     int absoluteValeMinute = _minute; //minuteValue
     int minuteElojel = 1;
     
@@ -56,11 +56,14 @@
         minuteElojel = 1;
     }
     
+    //We move the minute ticker by 6 degree every time a minte is passed
     double angleForMinute = (absoluteValeMinute * minuteElojel * 6) * (M_PI/180); //step is 6 degree
+    
+    //Calculate the right angle of the minute ticker, and its length will be 90 pixels
     double endxMinute = cos(angleForMinute) * 90 + image.size.width / 2;
     double endYMinute = sin(angleForMinute) * 90 + image.size.width / 2;
     
-    CGContextMoveToPoint(cotext, endxMinute, endYMinute); //((image.size.width/2), 60
+    CGContextMoveToPoint(cotext, endxMinute, endYMinute); 
     CGContextAddLineToPoint(cotext, image.size.width/2, image.size.height / 2);
     CGContextSetStrokeColorWithColor(cotext, [color CGColor]);
     /////////////////////////
@@ -78,17 +81,17 @@
         secondElojel = 1;
     }
     
+    //Move the second ticker by 6 degree
     double angleForSecond = (absoluteValueSeconds * secondElojel * 6) * (M_PI/180);
+    
+    //Make it 100 pixel long, so we can see size difference beside moving difference
     double endXSecond = cos(angleForSecond) * 100 + image.size.width / 2;
     double endYSecond = sin(angleForSecond) * 100 + image.size.height / 2;
     CGContextMoveToPoint(cotext, endXSecond, endYSecond);
     CGContextAddLineToPoint(cotext, image.size.width/2, image.size.height/2);
     
-    // CGContextConcatCTM(cotext, transform);
     CGContextStrokePath(cotext);
-    //
-    //Long clock ticcker thing
-    //Creat the new image
+    //Creat the new image and we draw the context we just created on the image
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return newImage;
@@ -96,6 +99,7 @@
     //return nil;
 }
 
+//This function gets the system time and updates the hour/minute/second values when we draw the clock
 -(void)updateTime{
     NSDate * now = [NSDate date];
     NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
